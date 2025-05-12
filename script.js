967ec8c1c95008843a706c99272fb5a362bd20b5
anubhutiyadav <anubhutiyadav@gmail.com>
2025-05-12 20:56:15 +0530

    update

---
+++ script.js
@@ -0,0 +1,63 @@
+// output
+
+// 1_console.log()
+// 2 prompt
+// 3 alert
+// 4 document.write()
+
+console.log('hello my first program in javascript')
+// alert('hello')
+prompt('what is your age')
+document.write('hello this is my javascript code')
+
+// VARIABLE
+// container which store data
+
+// let ,const and var 
+
+let x=3;
+x=5;
+
+console.log(x)
+document.write(x)
+
+// rules for varibale 
+// 1) Number,alphabest ,$,_,Ksensitive
+
+const Y=3;
+const username="Mukund";
+
+const $usernam1="amit"
+const _surname="kumar"
+const USERNAME="sunday"
+console.log(username,USERNAME)
+const uername=prompt('what is your Name?');
+const userage=prompt("what is your age?");
+
+console.log(userage,uername)
+
+const heading=document.getElementsByClassName("name")
+console.log(heading)
+
+heading.innerText='hye'+uername+"welcome to my site your age is "+userage+"your r eligible"
+// const 1username 
+
+// operator -long topic
+
+// variable ,javascript output,headin innertext,javascript ,javascript html add ways ,html basic things // output
+
+// 1_console.log()
+// 2 prompt
+// 3 alert
+// 4 document.write()
+
+console.log('hello my first program in javascript')
+// alert('hello')
+prompt('what is your age')
+document.write('hello this is my javascript code')
+
+// variable
+
+// container which store data
+
+// let ,const and var 
